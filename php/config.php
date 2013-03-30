24cda8a706b149de8139ea61f72375ff0d4b6c35
Marek Mahut <mmahut@redhat.com>
2013-03-30 10:27:52 +0100

    Use the same database as we tell in the documentation.

--- php/config.php
+++ php/config.php
@@ -6,7 +6,7 @@ $config['site']['name'] = 'Openshift Statusnet';
 $config['site']['server'] = $_ENV['OPENSHIFT_APP_DNS'];
 $config['site']['path'] = false; 
 
-$config['db']['database'] = 'mysqli://' . $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'] . '@' . $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT'] . '/' . $_ENV['OPENSHIFT_APP_NAME'];
+$config['db']['database'] = 'mysqli://' . $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'] . '@' . $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT'] . '/statusnet';
 
 $config['db']['type'] = 'mysql';
 
